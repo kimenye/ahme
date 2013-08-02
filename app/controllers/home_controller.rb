603883f14f1e9ce478e40424960886b866935197
Trevor Kimenye <kimenye@gmail.com>
2013-08-02 06:40:44 +0300

    added posts and categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   before_filter :authenticate_google_apps_user!
   def index
     @announcements = Announcement.all
-    @posts = Post.order("created_at DESC")
+
+    if params.has_key? (:category)
+      @posts = Post.joins(:categories).where('categories_posts.category_id = ?', params[:category]).order('posts.created_at desc')
+    else
+      @posts = Post.order("created_at DESC")
+    end
   end
 end
